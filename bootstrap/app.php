5abf775e2386790d2b03df9b455631b252dc5bcc
Julz <julzpalomiano@gmail.com>
2016-08-16 15:19:20 +0800

    first commit

---
+++ bootstrap/app.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => ''
+        ]
+    ],
+    
+]);
+
+$container = $app->getContainer();
+
+// LOAD ILLUMINATE DATABASE SUPPORT
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
+};
+// LOAD TWIG VIEW TEMPLATE
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views/', [
+        'cache' => false,
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $view;
+};
+
+$container['HomeController'] = function($container) {
+    return new \App\Controllers\HomeController($container);
+};
+
+require __DIR__ . '/../app/routes.php';
